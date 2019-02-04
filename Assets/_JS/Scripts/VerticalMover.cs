72b5f64aab083fc938701038ccee8e60722ed7b1
Jan Schmitz <jschm@DESKTOP-JSIL51U>
2019-02-04 17:20:18 +0100

    create repo

---
+++ Assets/_JS/Scripts/VerticalMover.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalMover : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float ForwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       
+
+        if ( Input.GetKey("right"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("left"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("up"))
+        {
+            rb.AddForce(0, 0, ForwardForce * Time.deltaTime);
+        }
+
+        if (Input.GetKey("down"))
+        {
+            rb.AddForce(0, 0, -ForwardForce * Time.deltaTime);
+        }
+
+    }
+}
